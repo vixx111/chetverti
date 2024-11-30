0a6ff68b1c6fe0c29407743e61efee58e7ac7651
Admin <Admin@DESKTOP-UGP0C2G>
2024-11-30 09:51:30 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,17 @@
+﻿    {
+        CheckPoint(2.2, -6); 
+        CheckPoint(5.3, 3);  
+        CheckPoint(-0.8, -4); 
+    }
+
+    static void CheckPoint(double x, double y)
+    {
+        if (x > 0 && y < 0)
+        {
+            Console.WriteLine($"Точка ({x}, {y}) лежит в четвертой четверти.");
+        }
+        else
+        {
+            Console.WriteLine($"Точка ({x}, {y}) не лежит в четвертой четверти.");
+        }
+    }
